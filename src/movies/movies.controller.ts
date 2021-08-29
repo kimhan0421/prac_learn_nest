6f080b2cd71f777e1ce00ca9bb11ee0ae8eabe10
hansol <kimhan0421@naver.com>
2021-08-29 16:43:17 +0900

    Feat: 2.3 Movies Service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,31 +1,30 @@
 import { Body, Patch, Query } from '@nestjs/common';
 import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
-  @Get()
-  getAll() {
-    return 'This will return all movies';
-  }
+  constructor(private readonly moviesService: MoviesService) {}
 
-  @Get('search')
-  serch(@Query('year') searchingYear: string) {
-    return `we ar searching for a movie ${searchingYear}`;
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get('/:id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
   remove(@Param('id') movieId: string) {
-    return `This will delete movie: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
